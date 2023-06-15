aecadb898960fda82e721ff41b29e2a01346c14d
Eriqo90AW <eriqo.arief@ui.ac.id>
2023-06-15 15:52:13 +0700

    finished login and register activity

--- Frontend/app/src/main/java/com/eriqoariefjsleeprj/frontend/LoginActivity.java
+++ Frontend/app/src/main/java/com/eriqoariefjsleeprj/frontend/LoginActivity.java
@@ -1,14 +1,101 @@
 package com.eriqoariefjsleeprj.frontend;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.eriqoariefjsleeprj.frontend.model.User;
+import com.eriqoariefjsleeprj.frontend.request.BaseApiService;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
+    private Retrofit retrofit;
+    private BaseApiService retrofitInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(LandingActivity.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        retrofitInterface = retrofit.create(BaseApiService.class);
+
+        findViewById(R.id.login_backButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        findViewById(R.id.login_loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleLogin();
+            }
+        });
+
+    }
+
+    private void handleLogin() {
+        EditText userEdit = findViewById(R.id.login_userInput);
+        EditText passEdit = findViewById(R.id.login_passInput);
+        Button loginBtn = findViewById(R.id.login_loginButton);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap<String, String> data = new HashMap<>();
+
+                data.put("username", userEdit.getText().toString());
+                data.put("password", passEdit.getText().toString());
+
+                Call<User> call = retrofitInterface.executeLogin(data);
+
+                call.enqueue(new Callback<User>() {
+                    @Override
+                    public void onResponse(Call<User> call, Response<User> response) {
+                        if(response.code() == 200){
+                            User result = response.body();
+                            if(result.getUsername() == null){
+                                Toast.makeText(LoginActivity.this, "Wrong username/password", Toast.LENGTH_SHORT).show();
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+
+                                builder.setTitle(result.getUsername());
+                                builder.setMessage(result.getEmail());
+                                builder.show();
+                            }
+                        }else if (response.code() == 400){
+                            Toast.makeText(LoginActivity.this, "Wrong username/password", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<User> call, Throwable t) {
+                        Toast.makeText(LoginActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        });
+
     }
 }
--- Frontend/app/src/main/java/com/eriqoariefjsleeprj/frontend/RegisterActivity.java
+++ Frontend/app/src/main/java/com/eriqoariefjsleeprj/frontend/RegisterActivity.java
@@ -1,14 +1,93 @@
 package com.eriqoariefjsleeprj.frontend;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.eriqoariefjsleeprj.frontend.request.BaseApiService;
+import com.eriqoariefjsleeprj.frontend.request.RegistrationData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegisterActivity extends AppCompatActivity {
+    private Retrofit retrofit;
+    private BaseApiService retrofitInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(LandingActivity.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        retrofitInterface = retrofit.create(BaseApiService.class);
+
+        findViewById(R.id.register_backButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        findViewById(R.id.register_registerButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleRegister();
+            }
+        });
+    }
+
+    private void handleRegister() {
+        EditText userEdit = findViewById(R.id.register_userInput);
+        EditText emailEdit = findViewById(R.id.register_emailInput);
+        EditText passEdit = findViewById(R.id.register_passInput);
+        Button registerBtn = findViewById(R.id.register_registerButton);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(userEdit.getText().toString().equals("") ||
+                        emailEdit.getText().toString().equals("") ||
+                        passEdit.getText().toString().equals("")){
+                    Toast.makeText(RegisterActivity.this, "Please fill out the form", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                RegistrationData data = new RegistrationData(
+                        userEdit.getText().toString(),
+                        emailEdit.getText().toString(),
+                        passEdit.getText().toString()
+                );
+                Call<Void> call = retrofitInterface.executeRegister(data);
+
+                call.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if(response.code() == 200){
+                            Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_LONG).show();
+                        }else if(response.code() == 400){
+                            Toast.makeText(RegisterActivity.this, "Failed to register", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Toast.makeText(RegisterActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        });
     }
 }
