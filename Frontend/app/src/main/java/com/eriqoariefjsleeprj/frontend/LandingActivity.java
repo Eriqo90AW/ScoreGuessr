e77cec192ca90ee65ebe7a4e9c438e81dd5f67e5
Eriqo90AW <eriqo.arief@ui.ac.id>
2023-06-15 14:06:53 +0700

    finished landing page

--- Frontend/app/src/main/java/com/eriqoariefjsleeprj/frontend/LandingActivity.java
+++ Frontend/app/src/main/java/com/eriqoariefjsleeprj/frontend/LandingActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
---
+++ Frontend/app/src/main/java/com/eriqoariefjsleeprj/frontend/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.eriqoariefjsleeprj.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+}
